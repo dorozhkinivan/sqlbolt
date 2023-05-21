7481a542ad45148029e921744e127e78473ed898
Ivan <dorozhkinivan123@gmail.com>
2023-05-22 01:42:41 +0300

    initial commit

---
+++ 1.sql
@@ -0,0 +1,16 @@
+-- https://sqlbolt.com/lesson/select_queries_introduction
+
+
+SELECT title FROM movies;
+
+
+SELECT director FROM movies;
+
+
+SELECT title, director FROM movies;
+
+
+SELECT title, year FROM movies;
+
+
+SELECT * FROM movies;
